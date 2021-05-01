9ebb7c66198f56e3bc2673b8ec820c49ec24e815
Luis Torres <Luis@Torres.com>
2021-05-01 10:02:49 -0500

    Comentarios, sentencias, variables, constantes y uso de let

---
+++ script.js
@@ -0,0 +1,91 @@
+alert('Hola Mundo');
+
+//Comentarios
+
+//Comentario de linea
+
+/*Comentario de mas
+ de una linea */
+
+
+
+ //Sentencias
+
+ //alert('Hola'); alert('Mundo');
+
+ //alert('Hola');
+ //alert('Mundo');
+
+ //alert('Hola')
+ //alert('Mundo')
+
+ //alert(3 + 1 + 2);
+
+ //Variables
+
+
+ let mensaje;
+ mensaje = "Hola Mundo";
+ alert(mensaje)
+
+ let a = 5;
+ let b = 6;
+ let suma = a + b;
+ console.log("El resultado de la suma es: ",suma);
+
+ let nombre = "Roberto";
+ let nombre2 = nombre;
+
+ nombre2 = "Carlos";
+ console.log("El nombre uno es: ",nombre );
+ console.log("El nombre dos es: ",nombre2 );
+
+
+//Nombrar variables con camel case
+nombreUno = "Roberto";
+nombreDos = "Carlos";
+
+//Nombrar como underscore
+nombre_uno = "Roberto";
+nombre_dos = "Carlos";
+
+//Nombres no comunes
+let $ = 1;
+let _ = 2;
+
+//No puede nombrar variables
+/*let 1a = 3;
+let primer-nombre = 4;*/
+
+"use strict"
+mensaje = "Hola Mundo";
+
+//Constantes
+
+const pi =3.141624;
+const COLOR_ROJO = "#FFFFF";
+pi_dos = pi;
+pi_dos = 7;
+console.log(pi);
+console.log(pi_dos);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
